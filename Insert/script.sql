9a7e8a262005fe5de9f92e7274fc8bf651ff1456
Hhhh Jjjj <didghwns0514@gmail.com>
2021-05-31 23:06:55 +0900

    new learning(Insert): 2021.05.31

---
+++ Insert/script.sql
@@ -0,0 +1,34 @@
+-- select opentutorials schema
+\! echo "use opentutorials schema!"
+USE opentutorials;
+\! echo ""
+
+-- show tables in schema
+\! echo "show tables!"
+SHOW TABLES;
+\! echo ""
+
+-- show row profile
+\! echo "describe the topic table!"
+DESC topic;
+\! echo ""
+
+-- insert a row(data) in the table
+/* 
+    1) id is a skip! 
+    2) tabale parameters must be wrapped with VAULES()
+    3) NOW() is a function to call and get current time-date
+    4) '  ' + \ will allow you to write sql on the next line as one
+*/
+\! echo "create a row into the table!"
+INSERT INTO topic (title, description, created, author, profile)  \
+            VALUES('MySQL', 'MySQL is...', NOW(), 'HJ Yang', 'developer');
+INSERT INTO topic (title, description, created, author, profile)  \
+            VALUES('SQL Server', 'SQL Server is...', NOW(), 'duruwa', 'data admin');
+\! echo ""
+
+\! echo "reading a row in the table!"
+\! echo "- select all data"
+-- selecting all the data from topic
+SELECT * FROM topic;
+\! echo ""
